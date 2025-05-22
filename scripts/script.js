d73bb0efe76d7c404230a76dcea60b2c96f43979
lowelshem <naranjolb@students.national-u.edu.ph>
2025-05-22 21:01:15 +0800

    cursor effect

--- scripts/script.js
+++ scripts/script.js
@@ -184,3 +184,17 @@ function setupDiscoveryForm() {
     document.querySelector('.file-name').textContent = 'No file chosen';
   });
 }
+
+
+// Water Cursor Effect typshi
+document.addEventListener("mousemove", (e) => {
+  const ripple = document.createElement("div");
+  ripple.className = "ripple";
+  ripple.style.left = `${e.clientX}px`;
+  ripple.style.top = `${e.clientY}px`;
+  document.body.appendChild(ripple);
+
+  setTimeout(() => {
+    ripple.remove();
+  }, 1000); //animation
+});
